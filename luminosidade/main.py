b95c2c4db8820cf28f1282209fc0f28881237453
VINÍCIUS JOSÉ LOPES CARDOSO <24122805@CFP127ALUNO.LOCAL>
2025-08-26 11:18:43 -0300

    FIX simulacao luminosidade

--- luminosidade/main.py
+++ luminosidade/main.py
@@ -1,7 +1,7 @@
 from machine import Pin, ADC
 import time
 
-PINO_LDR = 4
+PINO_LDR = 34
 PINO_LED_ESCURO = 23
 
 sensor_ldr = ADC(Pin(PINO_LDR))
@@ -22,9 +22,9 @@ while True:
     
     if valor_luminosidade < LIMIAR_ESCURO:
         
+        print("Ambiente claro! LED Apagado.")
+        led_escuro.value(0)
+    else:
         print("Ambiente escuro! Acendendo o LED.")
         led_escuro.value(1)
-    else:
-        print("Ambiente claro_-LED Apagado.")
-        led_escuro.value(0)
     time.sleep(1)
